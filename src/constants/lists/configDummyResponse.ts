dc878a57cd1bf8e99fee25a6fda0f3f6e4385a8d
김상준 <ingyoe64@gmail.com>
2025-09-10 11:55:42 +0900

    [Feat] 코드 하이라이팅 스타일 추가 및 더미 리스폰스 모음 파일 추가

---
+++ src/constants/lists/configDummyResponse.ts
@@ -0,0 +1,233 @@
+export const DUMMY_IMAGE_RESPONSE = [
+        { "idx": 101, "create_at": "2025-09-01T09:10:00Z", "url": "https://picsum.photos/id/1011/600/400" },
+        { "idx": 102, "create_at": "2025-09-01T12:20:00Z", "url": "https://picsum.photos/id/1025/600/400" },
+        { "idx": 103, "create_at": "2025-09-02T08:15:00Z", "url": "https://picsum.photos/id/1035/600/400" },
+        { "idx": 104, "create_at": "2025-09-02T15:45:00Z", "url": "https://picsum.photos/id/1043/600/400" },
+        { "idx": 105, "create_at": "2025-09-03T10:05:00Z", "url": "https://picsum.photos/id/1052/600/400" },
+        { "idx": 106, "create_at": "2025-09-03T17:30:00Z", "url": "https://picsum.photos/id/1060/600/400" },
+        { "idx": 107, "create_at": "2025-09-04T09:40:00Z", "url": "https://picsum.photos/id/1074/600/400" },
+        { "idx": 108, "create_at": "2025-09-04T18:25:00Z", "url": "https://picsum.photos/id/1084/600/400" },
+        { "idx": 109, "create_at": "2025-09-05T07:55:00Z", "url": "https://picsum.photos/id/109/600/400" },
+        { "idx": 110, "create_at": "2025-09-05T21:10:00Z", "url": "https://picsum.photos/id/110/600/400" },
+        { "idx": 111, "create_at": "2025-09-06T11:20:00Z", "url": "https://picsum.photos/id/111/600/400" },
+        { "idx": 112, "create_at": "2025-09-06T16:45:00Z", "url": "https://picsum.photos/id/112/600/400" },
+        { "idx": 113, "create_at": "2025-09-07T09:35:00Z", "url": "https://picsum.photos/id/113/600/400" },
+        { "idx": 114, "create_at": "2025-09-07T14:55:00Z", "url": "https://picsum.photos/id/114/600/400" },
+        { "idx": 115, "create_at": "2025-09-08T08:45:00Z", "url": "https://picsum.photos/id/115/600/400" },
+        { "idx": 116, "create_at": "2025-09-08T19:10:00Z", "url": "https://picsum.photos/id/116/600/400" },
+        { "idx": 117, "create_at": "2025-09-09T07:25:00Z", "url": "https://picsum.photos/id/117/600/400" },
+        { "idx": 118, "create_at": "2025-09-09T10:30:00Z", "url": "https://picsum.photos/id/118/600/400" },
+        { "idx": 119, "create_at": "2025-09-09T13:50:00Z", "url": "https://picsum.photos/id/119/600/400" },
+        { "idx": 120, "create_at": "2025-09-09T18:15:00Z", "url": "https://picsum.photos/id/120/600/400" }
+]
+
+export const DUMMY_POST_LIST_RESPONSE = [
+    {   
+        id: 1,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-01.png",
+        title: "네이버를 분석해보았다",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 2,
+        category: 2,
+        thumbnail: "/images/picture/img-dummy-thumbnail-02.png",
+        title: "배달의 민족에 대한 고찰(1)",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 3,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-03.png",
+        title: "테스팅 코드 에디터 만들어보기",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 4,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-04.png",
+        title: "모바일 청접장 서비스 개발기 (1)",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {   
+        id: 5,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-05.png",
+        title: "스타벅스 이제 배민도 가능하데용",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 6,
+        category: 2,
+        thumbnail: "/images/picture/img-dummy-thumbnail-06.png",
+        title: "토스 창립이 벌써 10주년?!",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 7,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-07.png",
+        title: "도깨비 나오긴 하나",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 8,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-08.png",
+        title: "모바일 청접장 서비스 개발기 (1)",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 9,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-09.png",
+        title: "네이버의 복지를 파보았다",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+    {
+        id: 10,
+        category: 1,
+        thumbnail: "/images/picture/img-dummy-thumbnail-010.png",
+        title: "카카오 아지트 둘러보기",
+        desc: "항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.\n공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.\n과연 여기 위치해도 괜찮은 것일까요?\n전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.\n그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.",
+        created_at: "2025-02-02"
+    },
+];
+
+export const DUMMY_POST_RESPONSE = {
+    "title": "네이버는 왜이럴까?",
+    "thumbnail": "/images/picture/img-dummy-thumbnail-01.png",
+    "contents": [
+    [
+        {
+            "id": "1757394528060",
+            "type": 0,
+            "title": "개선하고자 했던 부분",
+            "subtitle": "Portfolio",
+            "content": "<p><span>항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.</span><br><span>공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.</span><br><span>과연 여기 위치해도 괜찮은 것일까요?</span><br><br><span>전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.</span><br><br><span>그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.</span></p>",
+            "imageUrl": ""
+        },
+        {
+            "id": "1757394531258",
+            "type": 1,
+            "title": "",
+            "subtitle": "",
+            "content": "<p>새 블록</p>",
+            "imageUrl": "/images/picture/img-dummy-thumbnail-02.png"
+        }
+    ],
+    [
+        {
+            "id": "1757430364782",
+            "type": 1,
+            "title": "",
+            "subtitle": "",
+            "content": "<p>새 블록</p>",
+            "imageUrl": "/images/picture/img-dummy-thumbnail-01.png"
+        }
+    ],
+    [
+        {
+            "id": "1757394533499",
+            "type": 0,
+            "title": "개요",
+            "subtitle": "",
+            "content": "<p><span>항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.</span><br><span>공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.</span><br><span>과연 여기 위치해도 괜찮은 것일까요?</span><br><br><span>전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의  <strong>컴포넌트</strong>  때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.</span><br><br><span>그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.</span></p>",
+            "imageUrl": ""
+        }
+    ],
+    [
+        {
+            "id": "1757394509107",
+            "type": 2,
+            "title": "코드 조각",
+            "subtitle": "",
+            "content": "<p>&lt;div&gt;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;asdasd</p><p>&nbsp;&nbsp;&nbsp;&nbsp;asdasdasd</p><p>&lt;/div&gt;</p><p></p><p>&lt;script&gt;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;const nextConfig: NextConfig = {</p><p>&nbsp; &nbsp; webpack: (config) =&gt; {</p><p>&nbsp; &nbsp; &nbsp; &nbsp; // 기존 Webpack 설정을 확장합니다.</p><p>&nbsp; &nbsp; &nbsp; &nbsp; config.resolve.alias = {</p><p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ...config.resolve.alias,</p><p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; \"@\": path.resolve(__dirname, \"src\"), // '@'를 src 디렉토리로 매핑</p><p>&nbsp; &nbsp; &nbsp; &nbsp; };</p><p>&nbsp; &nbsp; &nbsp; &nbsp; // 수정된 config 반환</p><p>&nbsp; &nbsp; &nbsp; &nbsp; return config;</p><p>&nbsp; &nbsp; },</p><p>&nbsp; &nbsp; /* 기타 설정들 */</p><p>};</p><p>&lt;/script&gt;</p>",
+            "imageUrl": ""
+        },
+        {
+            "id": "1757394526973",
+            "type": 0,
+            "title": "설명",
+            "subtitle": "",
+            "content": "<p><span>항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.</span><br><span>공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.</span><br><span>과연 여기 위치해도 괜찮은 것일까요?</span><br><br><span>전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.</span><br><br><span>그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.</span></p>",
+            "imageUrl": ""
+        }
+    ],
+    [
+        {
+            "id": "1757429135698",
+            "type": 2,
+            "title": "코드 조각",
+            "subtitle": "",
+            "content": "<p>&lt;div&gt;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;asdasd</p><p>&nbsp;&nbsp;&nbsp;&nbsp;asdasdasd</p><p>&lt;/div&gt;</p><p></p><p>&lt;script&gt;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;const nextConfig: NextConfig = {</p><p>&nbsp; &nbsp; webpack: (config) =&gt; {</p><p>&nbsp; &nbsp; &nbsp; &nbsp; // 기존 Webpack 설정을 확장합니다.</p><p>&nbsp; &nbsp; &nbsp; &nbsp; config.resolve.alias = {</p><p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ...config.resolve.alias,</p><p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; \"@\": path.resolve(__dirname, \"src\"), // '@'를 src 디렉토리로 매핑</p><p>&nbsp; &nbsp; &nbsp; &nbsp; };</p><p>&nbsp; &nbsp; &nbsp; &nbsp; // 수정된 config 반환</p><p>&nbsp; &nbsp; &nbsp; &nbsp; return config;</p><p>&nbsp; &nbsp; },</p><p>&nbsp; &nbsp; /* 기타 설정들 */</p><p>};</p><p>&lt;/script&gt;</p>",
+            "imageUrl": ""
+        }
+    ]
+],
+    "create_at": "2025.09.09"
+}
+
+export const DUMMY_POST_RESPONSE_2 = {
+    "title": "네이버는 왜이럴까?",
+    "thumbnail": "/images/picture/img-dummy-thumbnail-02.png",
+    "contents": [
+        [
+            {
+                "id": "1757394528060",
+                "type": 0,
+                "title": "개선하고자 했던 부분",
+                "subtitle": "Portfolio",
+                "content": "<p><span>항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.</span><br><span>공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.</span><br><span>과연 여기 위치해도 괜찮은 것일까요?</span><br><br><span>전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.</span><br><br><span>그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.</span></p>",
+                "imageUrl": ""
+            },
+            {
+                "id": "1757394531258",
+                "type": 1,
+                "title": "",
+                "subtitle": "",
+                "content": "<p>새 블록</p>",
+                "imageUrl": "/images/picture/img-dummy-thumbnail-02.png"
+            }
+        ],
+        [
+            {
+                "id": "1757394533499",
+                "type": 0,
+                "title": "개요",
+                "subtitle": "",
+                "content": "<p><span>항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.</span><br><span>공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.</span><br><span>과연 여기 위치해도 괜찮은 것일까요?</span><br><br><span>전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의  <strong>컴포넌트</strong>  때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.</span><br><br><span>그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.</span></p>",
+                "imageUrl": ""
+            }
+        ],
+        [
+            {
+                "id": "1757394509107",
+                "type": 0,
+                "title": "코드 조각",
+                "subtitle": "",
+                "content": "<pre><code>const nextConfig: NextConfig = {\n    webpack: (config) =&gt; {\n        // 기존 Webpack 설정을 확장합니다.\n        config.resolve.alias = {\n            ...config.resolve.alias,\n            \"@\": path.resolve(__dirname, \"src\"), // '@'를 src 디렉토리로 매핑\n        };\n        // 수정된 config 반환\n        return config;\n    },\n    /* 기타 설정들 */\n};</code></pre><p></p>",
+                "imageUrl": ""
+            },
+            {
+                "id": "1757394526973",
+                "type": 0,
+                "title": "설명",
+                "subtitle": "",
+                "content": "<p><span>항상 토이프로젝트를 만들다 보면 문득 고민에 빠지는 것들이 있었습니다.</span><br><span>공용 컴포넌트의 경우, 이 컴포넌트는 여러 곳에서 사용되지만, 모든 곳에서 쓰지는 않습니다.</span><br><span>과연 여기 위치해도 괜찮은 것일까요?</span><br><br><span>전역 사용 컴포넌트의 경우, 이 컴포넌트는 모든 곳에서 사용되긴 하는데, 하나의 컴포넌트 때문에 공용으로 사용하는 폴더에 넣는 것이 올바른 선택인지 고민됩니다.</span><br><br><span>그 고민을 해결해보고자 이번에는 FSD에 대해 알아보고자 합니다.</span></p>",
+                "imageUrl": ""
+            }
+        ]
+    ],
+    "create_at": "2025.09.09"
+}

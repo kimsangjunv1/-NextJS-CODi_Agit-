90b604e87061b94e7ecab78470fcca933fdbfdfa
김상준 <ingyoe64@gmail.com>
2025-10-09 00:48:31 +0900

    [Hotfix] cookie 관련 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 import Main from "@/components/layout/Main";
 import ListSection from "@/containers/init/ListSection";
 import { supabaseServer } from "@/utils/supabase/supabaseServer";
-import { GetPostLatestListResponseType, ApiHeaderResponseType } from "@/types/post.type";
+import { GetPostLatestListResponseType } from "@/types/post.type";
+import { ApiHeaderResponseType } from "@/types/common.type";
 
 export const revalidate = 60; // ISR: 60초마다 페이지 재생성
 
--- src/utils/supabase/supabaseServer.ts
+++ src/utils/supabase/supabaseServer.ts
@@ -3,31 +3,12 @@
 
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers'; // Next.js 14+에서 서버 쿠키 접근용
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-export async function supabaseServer() { // 함수 이름을 명확하게!
-    const cookieStore = await cookies(); // 동기 함수이므로 await 필요 없어요.
-
-    return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return cookieStore.getAll();
-                },
-                
-                setAll(cookiesToSet) {
-                    try {
-                        cookiesToSet.forEach(({ name, value, options }) =>
-                        cookieStore.set(name, value, options)
-                        );
-                    } catch (error) {
-                        // 이 에러 핸들링은 미들웨어에서 세션을 새로고침할 때 발생할 수 있는
-                        // 'cookies().set()' 호출 오류를 무시하기 위함입니다.
-                        // (주로 Server Component에서 setAll이 호출될 때)
-                    }
-                },
-            },
-        }
-    )
-};
+export async function supabaseServer() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    // cookies 옵션 제거
+  );
+}
